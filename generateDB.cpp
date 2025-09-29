cd31f1f0dff056b1201e87a52e0e0b5464a5c2e7
Hurdamert <lichengtx@gmail.com>
2025-09-29 16:40:05 -0500

    major functionality done, date change done, need to implement peak days

--- generateDB.cpp
+++ generateDB.cpp
@@ -3,15 +3,54 @@
 #include<fstream>
 #include<string>
 #include<random>
-#include<unordered_set>
+#include<unordered_map>
 using namespace std;
 
+
+//function to increment the date and time correctly (leap years not calculated)
+string incrementDateTime(int& year, int& month, int& day){
+    if(month == 12 && day == 31){
+        year++;
+        month = 1;
+        day = 1;
+    }
+    else{
+        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            if(day == 31){
+                day = 1;
+                month++;
+            }
+            else{
+                day++;
+            }
+        }
+        else if (month == 4 || month == 6 || month == 9 || month == 11){
+            if(day == 30){
+                day = 1;
+                month++;
+            }
+            else{
+                day++;
+            }
+        }
+        else{
+            if(day == 28){
+                day = 1;
+                month++;
+            }
+            else{
+                day++;
+            }
+        }
+        
+    }
+    return to_string(year) + "-" + to_string(month) + "-" + to_string(day);
+}
 
 int main(int argc, char* argv[]){
     //create outfile
     ofstream orders("./orders.csv");
     ofstream orderItems("./orderItems.csv");
-    double maxTotal = 1250000;
 
     //make vector of all items that can be sold
     vector<string> itemIndex = {"", "Classic Pearl Milk Tea", 
@@ -24,7 +63,7 @@ int main(int argc, char* argv[]){
          "Peach Tea w/ Honey Jelly", "Mango & Passion Fruit Tea", "Honey Lemonade", "Halo Halo", 
          "Wintermelon Lemonade", "Strawberry Coconut"};
     
-    unordered_set<string, double> itemToPrice = {
+    unordered_map<string, double> itemToPrice = {
         {"Classic Pearl Milk Tea", 5.80},
         {"Honey Pearl Milk Tea", 6.00},
         {"Coffee Creama", 6.50},
@@ -69,6 +108,10 @@ int main(int argc, char* argv[]){
     long itemQuantity;
     int employeeID;
     double subTotal;
+    double dayTotal;
+
+    random_device rd;
+    mt19937 gen(rd());
 
     int peakDaysLimit = 11000;
     int normalDaysLimit = 2700;
@@ -77,14 +120,39 @@ int main(int argc, char* argv[]){
     for(int i = 0; i < 65; i++){
         //simulate 7 days
         for(int j = 0; j < 7; j++){
-            
+            dayTotal = 0;
+
+            while(dayTotal < normalDaysLimit){
+                //randomly choose between 1-3 drinks and 1-2 quantity per order
+                subTotal = 0;
+                int numDrinks = (gen() % 3) + 1;
+                int itemQuantity = (gen() % 2) + 1;
+
+                for(int k = 0; k < numDrinks; k++){
+                    //choose random drink and add price to subTotal
+                    productID = (gen() % 24) + 1;
+                    string drink = itemIndex[productID];
+                    double price = itemToPrice.at(drink);
+
+                    subTotal += price * itemQuantity;
+                    
+                    //add to orderItems
+                    orderItems << productID << "," << orderID << "," << itemQuantity << "\n";
+                }
+                
+                employeeID = (gen() % 5) + 1;
+                //add to orders
+                orders << employeeID << "," << subTotal << "," << timestamp << "\n";
+
+                orderID++;
+                dayTotal += subTotal;
+            }
+            //change time
+            timestamp = incrementDateTime(year, month, day);
         }
     }
 
     orders.close();
+    orderItems.close();
     return 0;
-}
-
-string incrementDateTime(int year, int month, int day){
-
 }
