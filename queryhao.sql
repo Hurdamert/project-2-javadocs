815633141dab400dca293e02b35cb42973ed163b
Hao Lin <haolin0211@tamu.edu>
2025-09-29 13:23:40 -0500

    base queries

---
+++ queryhao.sql
@@ -0,0 +1,16 @@
+SELECT * FROM ORDERS;
+
+SELECT * FROM EMPLOYEE;
+
+SELECT * FROM addons;
+
+SELECT * FROM Ingredients;
+
+SELECT * FROM order_items;
+
+SELECT * FROM PRODUCTS;
+
+SELECT * FROM ProductIngredients;
+
+SELECT * FROM categories;
+
