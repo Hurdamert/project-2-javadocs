7ecb79b11ee898cf9794f29ca9a48e95880da7f6
Hurdamert <lichengtx@gmail.com>
2025-09-29 15:08:17 -0500

    changed generateDB file to include orders and orderItems

--- generateDB.cpp
+++ generateDB.cpp
@@ -5,33 +5,18 @@ using namespace std;
 
 
 int main(int argc, char* argv[]){
-    //create outfiles
-    ofstream addOns("./addOns.csv");
-    ofstream categories("./categories.csv");
+    //create outfile
+    ofstream orders("./orders.csv");
+    ofstream orderItems("./orderItems.csv");
+    double maxTotal = 1250000;
 
     //add headers to csv
-    string addOnsHeader = "add_on_id, add_on_name, add_on_price, category_id";
-    string categoriesHeader = "category_id, category_name";
+    string ordersHeader = "employee_id,sub_total,date_time";
+    string orderItemsHeader = "product_id,order_id,item_quantity";
 
-    addOns << addOnsHeader << "\n";
-    categories << categoriesHeader << "\n";
+    orders << ordersHeader << "\n";
+    orderItems << orderItemsHeader << "\n";
 
-    //add addons
-    addOns << 0 << ",Pearls," << 0.4 << "," << 0 << "\n";
-    addOns << 1 << ",Honey Jelly," << 0.3 << "," << 0 << "\n";
-    addOns << 2 << ",Pudding," << 0.4 << "," << 0 << "\n";
-    addOns << 3 << ",Ice Cream," << 0.5 << "," << 0 << "\n";
-    addOns << 4 << ",Coffee Jelly," << 0.6 << "," << 0 << "\n";
-    addOns << 5 << ",Crystal Boba," << 0.5 << "," << 0 << "\n";
-
-    //add categories
-    categories << 1 << ",Milky Series" << "\n";
-    categories << 2 << ",Fresh Milk" << "\n";
-    categories << 3 << ",Fruit Drinks" << "\n";
-    categories << 1 << ",Non-Caffeinated" << "\n";
-    
-    addOns.close();
-    categories.close();
-
+    orders.close();
     return 0;
 }
