3d381acb72f671c845095921c3981e1fdeb41010
Evan Ganske <evanganske1@gmail.com>
2025-10-22 21:02:51 -0500

    Added documentation for EmployeeController, Ingredient, dbSetup

--- src/main/java/com/example/dbSetup.java
+++ src/main/java/com/example/dbSetup.java
@@ -2,6 +2,11 @@ package com.example;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
+/**
+  * The dbSetup class loads a .env and populates a username and password 
+  * a customer orders an item at the boba shop
+  * @author Evan Ganske
+*/
 public final class dbSetup {
     private static final Dotenv dotenv = Dotenv.load();
 
